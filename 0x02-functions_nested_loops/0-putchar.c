8ee7fd8db0b1cfe9e4687646b24c0e0d317bfb6e
Katungwa98 <iankioko9@gmail.com>
2022-12-15 04:11:41 -0800

    nested loops and funcs

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - print string
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
+
+
