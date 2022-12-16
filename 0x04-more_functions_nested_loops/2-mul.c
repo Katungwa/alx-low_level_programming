46f94b9256dae90610b5046673ef93e8da331e44
Katungwa98 <iankioko9@gmail.com>
2022-12-16 14:46:19 -0800

    more functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,26 +2,9 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: fir#include "main.h"
- *print_numbers - print 0123456789
- *Return: void
-*/
-void print_numbers(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}
-
-/**
- * Last integer
- * @b: second integer
- * Return: addition of a and b
+* @a: first integer
+* @b: second integer
+* Return: multiplication of a and b
 */
 int mul(int a, int b)
 {
