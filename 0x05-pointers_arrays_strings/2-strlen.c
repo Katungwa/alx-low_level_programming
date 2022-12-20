72db6e2bf030946ac6dfd75ef0cd5174c99ae2fb
Katungwa98 <iankioko9@gmail.com>
2022-12-19 22:01:01 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen: length of a string
- * s: address of the string
+ * _strlen - length of a string
+ * @s: address of the string
  * return: int
  */
 int _strlen(char *s)
 {
 	int len;
 
-	len = sizeof(s)/sizeof(s[0]);
-	return len;
+	len = sizeof(*s) / sizeof(*s[0]);
+	return (len);
 }
