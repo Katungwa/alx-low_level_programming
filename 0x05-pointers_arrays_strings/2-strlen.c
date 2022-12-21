16ad2c2de04a5ea448fe156b52a227958b0b6cac
Katungwa98 <iankioko9@gmail.com>
2022-12-21 08:32:47 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 /**
- * _strlen - length of a string
+ * _strlen-length of a string
  * @s: address of the string
- * return: int
+ * Return: Always 0
  */
 int _strlen(char *s)
 {
