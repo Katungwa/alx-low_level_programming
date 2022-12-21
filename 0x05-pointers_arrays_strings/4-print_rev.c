4a4dc32f7bcfa03be0fb5798bc8a09d416482a50
Katungwa98 <iankioko9@gmail.com>
2022-12-21 09:29:45 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: The strin to be reversed
+ * Return : void
+ */
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
