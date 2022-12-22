db135efad091d6bd9bccc8ede1e295fad18498a6
Katungwa98 <iankioko9@gmail.com>
2022-12-21 21:38:05 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,13 @@
 void rev_string(char *s)
 {
 	/*@len:length of the string*/
-	int len;
+	int len, i;
 	char temp;/*@temp;: temporary variable to hold array elem*/
 	int middle;/*@middle: middle of a string*/
 
 	len = strlen(s);
 	middle = len / 2;
-	for (int i = 0; i < middle; i++)
+	for (i = 0; i < middle; i++)
 	{
 		temp = s[i];
 		s[i] = s[len - i - 1];
