3d1b4c6ce0d972b56cdd814bb05b97174775338c
Katungwa98 <iankioko9@gmail.com>
2022-12-22 03:03:59 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,16 +11,16 @@ void print_rev(char *s)
 	int len;
 	int middle;
 	int temp, i;
-	char str[] = *s;
+	
 
-	len = strlen(str);
+	len = strlen(s);
 	middle = len / 2;
 
 	for (i = 0; i < middle; i++)
 	{
-		temp = str[i];
-		str[i] = str[len - i - 1];
-		str[len - i - 1] = temp;
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
-	puts(str);
+	puts(s);
 }
