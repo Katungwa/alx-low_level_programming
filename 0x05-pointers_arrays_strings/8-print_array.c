9863a7ea4bb27dd497ef5e93b8c301180aa8d118
Katungwa98 <iankioko9@gmail.com>
2022-12-22 22:29:27 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array - prints a certain number of elem in an array
+ * @a: The array to be manipulated
+ * @n: number of elements to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-
+void print_array(int *a, int n);
 
 
 
