ab638e5282721c463d16996c814c5b60139531b2
Katungwa98 <iankioko9@gmail.com>
2022-12-22 20:31:29 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints chars of a string starting with the first
+ * @str: The string to be printed
+ * Return - void
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str) - 1;
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+
+
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void swap_int(int *a,int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
 
 
 
