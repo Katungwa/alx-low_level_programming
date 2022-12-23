47fadca98b9311be05679629fb96039dbef472cc
Katungwa98 <iankioko9@gmail.com>
2022-12-22 21:57:04 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half -  prints second half of a given string
+ * @str: The string to be manipulated
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, half, len;
+
+	len = strlen(str) - 1;
+	if len % 2 == 0
+	{
+		half = len / 2;
+	}
+	else
+		half = (len - 1) / 2;
+	for (i = half; i < len; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
