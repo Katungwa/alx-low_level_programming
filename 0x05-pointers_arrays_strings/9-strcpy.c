42e29690de34d457d13a3cd89b065f7c9dcbfd27
Katungwa98 <iankioko9@gmail.com>
2022-12-23 21:37:32 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy copies a string to a buffer
+ * @scr: string pointer
+ * @dest: The buffer to hold the copied string
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	
+	return dest;
+}
