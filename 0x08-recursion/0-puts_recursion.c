c96d898936c223f731ab990aaa456398f37b3f40
Katungwa98 <iankioko9@gmail.com>
2023-01-04 22:54:49 -0800

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints out a string followed by a new line
+ * @s: a character pointer to the string of words
+ * Return - void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+This is a dir on the recursion subtopic
+
