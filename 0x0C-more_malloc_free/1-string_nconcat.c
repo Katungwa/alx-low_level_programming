75a478e944147dc4de28b3aee942d1dd95f2293f
Katungwa98 <iankioko9@gmail.com>
2023-01-14 23:25:11 -0800

    more malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (index = 0; s1[index]; index++)
 		len++;
 
-	s = malloc(len + 1);
+	s = malloc(sizeof(char) * (len + 1));
 
 	if (s == NULL)
 		return (NULL);
