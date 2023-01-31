b2162473a9d803d6608d13c546562f44c3d29df7
Katungwa98 <iankioko9@gmail.com>
2023-01-30 18:56:42 -0800

    linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	dup_str =strdup(str);
+	dup_str = strdup(str);
 	if (dup_str == NULL)
 	{
 		free(new);
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,4 +21,5 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
