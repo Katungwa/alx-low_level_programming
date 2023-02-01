8cd31895407142a29de16776ecd747bf777d04f5
Katungwa98 <iankioko9@gmail.com>
2023-01-31 18:58:00 -0800

    linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,9 +2,9 @@
 
 /**
  * add_node - adds node at the beginning of list
- * @head - The pointer to the address of first node
+ * @head: The pointer to the address of first node
  * @str: str param
- * @Return: pointer
+ * Return: pointer if successful
  */
 list_t *add_node(list_t **head, const char *str)
 {
