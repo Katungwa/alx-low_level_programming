c5bccca3e6b391860d9fb6195674430d8498e1f2
Katungwa98 <iankioko9@gmail.com>
2023-02-01 02:35:25 -0800

    print elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t i_node;
 
+	i_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (Nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+		{
+			printf("[%d] %s\n",h->len, h->str);
+		}
 		h = h->next;
-		i++;
+		i_node++;
 	}
-	return (i);
+	return (i_node);
 }
