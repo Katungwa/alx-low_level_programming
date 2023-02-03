dfcac40d93ea7d98feb881532e2bcf727af6ccf8
Katungwa98 <iankioko9@gmail.com>
2023-02-02 19:23:48 -0800

    free more lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to a node address
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head != NULL)
+	{
+		node = *head;
+		*head = node->next;
+		free(node);
+	}
+}
