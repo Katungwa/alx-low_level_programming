d35d19c879b1f2b1b3ab0bd7a97b79ecedbe99ce
Katungwa98 <iankioko9@gmail.com>
2023-02-02 23:00:38 -0800

    get nth node in list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,14 +11,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int i = 0;
 
-	if (!head)
-		return (NULL);
-
-	while (i < index)
+	while (i <= index && head)
 	{
 		node = head;
 		head = node->next;
 		i++;
+		
 	}
 	if (!node)
 		return (NULL);
